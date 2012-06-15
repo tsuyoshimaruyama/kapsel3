63f59fbc37c8b820fb620826f8123790accb0392
john <john@cheme.kyoto-u.ac.jp>
2012-06-15 16:58:49 +0900

    Minor Bug fix
    - correct B2 calculation

--- avs_process.cxx
+++ avs_process.cxx
@@ -1,9 +1,9 @@
 #include "avs_process.h"
-void process_avs_frame(const V_OPTION &vflag, const int &frame);
 void v_gold(double ww[DIM], const double nr[DIM], const double &rdist, 
 	    const double &B1, const double &B2);
 void get_image(const int &frame, const int &iso);
-void get_image_rotate(const int &frame, const int &npi);
+void get_rms(const int &frame);
+
 int main(int argc, char* argv[]){
   int pid;                // id of centered particle
   int *p_spec;            // species id for all particles
@@ -11,18 +11,16 @@ int main(int argc, char* argv[]){
   double *sp_slip;        // slip velocity for each species
   double *sp_slipmode;    // B2 coefficient
 
-  V_OPTION vflag;         // output flag for velocites
   UDFManager *udf_in;     // UDF input file
   
-  initialize(argc, argv, udf_in, pid, vflag);
+  rms[0] = rms[1] = rms[2] = rms[3] =  0.0;
+  initialize(argc, argv, udf_in, pid);
   get_system_data(udf_in, p_spec, sp_axis, sp_slip, sp_slipmode);
   initialize_avs();
 
   u = (double **)malloc(sizeof(double*)* DIM);
-  post_u = (double **)malloc(sizeof(double*)*DIM);
   for(int d = 0; d < DIM; d++){
     u[d] = alloc_1d_double(NX*NY*NZ);
-    post_u[d] = alloc_1d_double(CX*CY*CZ);
   }
   assert(pid < Nparticles);
   //janus axis of centered particle
@@ -42,63 +40,117 @@ int main(int argc, char* argv[]){
   //slip velocity of centered particle
   B1_real = sp_slip[p_spec[pid]];
   B2 = sp_slipmode[p_spec[pid]];
+  B2 = B2*B1_real;
+  UP = 2./3. * B1_real;
+  fprintf(stderr, "# UP = %g (%g)\n",UP,B1_real);
 
   for(int i = 0; i <= Num_snap; i++){
     setup_avs_frame();
     read_u();
     read_p(pid);
+    get_rms(i);
     get_image(i, 0);
-    get_image(i, 1);
     get_image(i, 2);
     clear_avs_frame();
   }
+  fprintf(stderr, "# Error: %8.5e %8.5e %8.5e %8.5e\n", 
+	  rms[0]/(double)Num_snap, 
+	  rms[1]/(double)Num_snap, 
+	  rms[2]/(double)Num_snap,
+	  rms[3]/(double)Num_snap);
   fclose(fluid_field);
   fclose(particle_field);
-  fclose(post_field);
 }
 
-inline void binary_write(double *a, FILE *fstream){
-  for(int k = 0; k < CZ; k++){
-    for(int j = 0; j < CY; j++){
-      for(int i = 0; i < CX; i++){
-	int im = linear_id(i, j, k, CX, CY, CZ);
-	float dmy = (float)a[im];
-	fwrite(&dmy, sizeof(float), 1, fstream);
-      }
-    }
-  }
-}
-
+/////
 void v_gold(double ww[DIM], const double nr[DIM], const double &rdist, 
 	    const double &B1, const double &B2){
   double dmy, cos_theta;
-  double jax[DIM];
+  double jax[DIM], nsin_theta[DIM];
   rigid_body_rotation(jax, e3, q0, BODY2SPACE);
   double nd1[DIM], nd2[DIM], nd3[DIM];
   double ar, ar2, ar3;
 
-  if(rdist > A){
+  if(rdist >= A + DX){
     cos_theta = jax[0]*nr[0] + jax[1]*nr[1] + jax[2]*nr[2];
-    ar = (A)/rdist;
+    ar = (A+DX)/rdist;
     ar2 = ar*ar;
     ar3 = ar*ar2;
 
     for(int d = 0; d < DIM; d++){
+      nsin_theta[d] = cos_theta * nr[d] - jax[d];
+    }
+
+    for(int d = 0; d < DIM; d++){
       nd1[d] = cos_theta * nr[d] - 1.0/3.0 * jax[d];
-      nd2[d] = cos_theta * (cos_theta * nr[d] - jax[d]);
+      nd2[d] = 2.0 * cos_theta * nsin_theta[d];
       nd3[d] = (3.0 * cos_theta * cos_theta - 1.0) * nr[d];
     }
     for(int d = 0; d < DIM; d++){
       ww[d] = B1 * ar3 * nd1[d] + 
-	B2/2.0 * ar2 * ( 2.0 * ar2 * nd2[d] + (ar2 - 1.0) * nd3[d] );
+	B2/2.0 * ar2 * ( ar2 * nd2[d] + (ar2 - 1.0) * nd3[d] );
     }
   }else{
     for(int d = 0; d < DIM; d++){
       ww[d] = 0.0;
     }
   }
+  for(int d = 0; d < DIM; d++){
+    assert(ww[d] == ww[d]);
+  }
 }
 
+void get_rms(const int &frame){
+  int ei[DIM], count;
+  double rgrid[DIM], nr[DIM], ww[DIM], df[DIM];
+  double e0, e1, e2, e3, dmy_r, scale, dmy_e;
+  e0 = e1 = e2 = e3 = 0.0;
+  count = 0;
+  for(int i = 0; i < NX; i++){
+    ei[0] = i;
+    for(int j = 0; j < NY; j++){
+      ei[1] = j;
+      for(int k = 0; k < NZ; k++){
+	ei[2] = k;
+
+	for(int d = 0; d < DIM; d++){
+	  rgrid[d] = (double)ei[d] * DX;
+	}
+	Distance(r0, rgrid, dmy_r, nr);
+	if(dmy_r >= A + DX){
+	  int im = (i * NY * NZ) + (j * NZ) + k;
+	  scale = dmy_r/(A + DX);
+	  scale *= scale;
+
+	  v_gold(ww, nr, dmy_r, B1_real, B2);
+	  for(int d = 0; d < DIM; d++){
+	    df[d] = (u[d][im] - ww[d])/UP;
+	  }
+	  dmy_e = v_inner_prod(df, df);
+
+	  e0 += dmy_e;
+	  e1 += dmy_e * scale;
+	  e2 += dmy_e * scale*scale;
+	  e3 += dmy_e * scale*scale*scale;
+	  count ++;
+	}
+      }
+    }
+  }
+  if(count > 0){
+    e0 = sqrt(e0 / (double)count);
+    e1 = sqrt(e1 / (double)count);
+    e2 = sqrt(e2 / (double)count);
+    e3 = sqrt(e3 / (double)count);
+  }
+  fprintf(stderr, "%d %8.5g %8.5g %8.5g %8.5g\n", frame, e0, e1, e2, e3);
+  if(frame > 0){
+    rms[0] += e0;
+    rms[1] += e1;
+    rms[2] += e2;
+    rms[3] += e3;
+  }
+}
 void point_id(const int &iso, const int &ii, const int &jj, const int &kk, int *ei){
   switch(iso){
   case 0:
@@ -119,13 +171,9 @@ void point_id(const int &iso, const int &ii, const int &jj, const int &kk, int *
   }
 }
 void get_image(const int &frame, const int &iso){
-  double rp[DIM], sp[DIM], cp[DIM], vv[DIM], ww[DIM], dmy;
-  int ei[DIM], edge[DIM];
+  double rp[DIM], vv[DIM], ww[DIM], dmy;
+  int ei[DIM], gi[DIM];
   int Nj, Nk;
-  int interpol_pid[8][DIM];
-  double interpol_pval[8];
-  double interpol_coeff[64];
-
   char dmy_path[256];
   FILE *fstream;
 
@@ -164,21 +212,24 @@ void get_image(const int &frame, const int &iso){
   double v01 = v_inner_prod(v0, ev);
   double v02 = v_inner_prod(v0, eu);
   double v03 = v_inner_prod(v0, ew);
-  double fdomain;
+  double rr1 = v_inner_prod(res0, ev);
+  double rr2 = v_inner_prod(res0, eu);
+  double rr3 = v_inner_prod(res0, ew);
+  double pdomain, fdomain;
 
   int ii = 0;
   for(int jj = -Nj; jj <= Nj; jj++){
     for(int kk = -Nk; kk <= Nk; kk++){
       point_id(iso, ii, jj, kk, ei);
+      for(int d = 0; d < DIM; d++){
+	gi[d] = ei[d] + r0_int[d];
+      }
+      PBC_ip(gi);
+      int im = (gi[0] * NY * NZ) + (gi[1] * NZ) + gi[2];
 
       dmy = 0.0;
       for(int d = 0; d < DIM; d++){
-	rp[d] = (double)ei[d] * DX;
-	sp[d] = r0[d] + rp[d];
-	sp[d] = fmod(sp[d] + lbox[d], lbox[d]);
-	edge[d] = (int)(sp[d]/DX);
-	cp[d] = sp[d] - edge[d]*DX;
-
+	rp[d] = (double)ei[d] * DX - res0[d];
 	dmy += rp[d]*rp[d];
       }
       dmy = sqrt(dmy);
@@ -186,19 +237,18 @@ void get_image(const int &frame, const int &iso){
 	rp[d] = (dmy > 0 ? rp[d] / dmy : 0.0);
       }
       v_gold(ww, rp, dmy, B1_real, B2);
-
-      if(dmy > A){
-	for(int d = 0; d < DIM; d++){
-	  init_interpol(interpol_pid, interpol_pval, interpol_coeff, u[d], edge);
-	  vv[d] = interpol(interpol_pid, interpol_pval, interpol_coeff, cp);
-	}
+      pdomain = Phi(dmy, A);
+      if(dmy >= A+DX){
 	fdomain = 1.0;
       }else{
-	for(int d = 0; d < DIM; d++){
-	  vv[d] = 0.0;
-	}
 	fdomain = 0.0;
       }
+      
+
+      for(int d = 0; d < DIM; d++){
+	vv[d] = fdomain*u[d][im];
+      }
+
 
       double vv1 = v_inner_prod(vv, ev);
       double vv2 = v_inner_prod(vv, eu);
@@ -209,78 +259,15 @@ void get_image(const int &frame, const int &iso){
       double ww3 = v_inner_prod(ww, ew);
 
       fprintf(fstream, 
-	      "%3d %3d %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g \n",
-	      jj, kk, dmy, vv1, vv2, vv3, ww1, ww2, ww3,
-	      fdomain*v01, fdomain*v02, fdomain*v03);
+	      "%3d %3d %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g  %8.5g %8.5g %8.5g %8.5g %8.5f\n",
+	      jj, kk, dmy, 
+	      vv1, vv2, vv3, 
+	      fdomain*ww1, fdomain*ww2, fdomain*ww3,
+	      fdomain*v01, fdomain*v02, fdomain*v03,
+	      rr1, rr2, rr3, 
+	      pdomain);
     }
   }
   fclose(fstream);
 }
-
-void get_image_rotate(const int &frame, const int &npi){
-  double rp[DIM], sp[DIM], cp[DIM], vv[DIM], ww[DIM], dmy;
-  int edge[DIM];
-  int interpol_pid[8][DIM];
-  double ei[DIM];
-  double interpol_pval[8];
-  double interpol_coeff[64];
-  double theta, cs, sn;
-
-  char dmy_path[256];
-  FILE *fstream;
-  sprintf(dmy_path, "%s/plots/wz-PI%d_%d.dat", AVS_dir, npi, frame);
-  fstream = filecheckopen(dmy_path, "w");
-  theta = M_PI / (double)npi;
-  cs = cos(theta);
-  sn = sin(theta);
-
-  double eu[DIM] = {cs, sn, 0.0};
-  double ev[DIM] = {-sn, cs, 0.0};
-  double ew[DIM] = {0.0, 0.0, 1.0};
-
-  int HN_UV = min(HCX, min(HCY, HCZ));
-  for(int jj = -HN_UV; jj <= HN_UV; jj++){
-    ei[0] = (double)jj * cs;
-    ei[1] = (double)jj * sn;
-    for(int kk = -HN_UV; kk <= HN_UV; kk++){
-      ei[2] = (double) kk;
-
-      dmy = 0.0;
-      for(int d = 0; d < DIM; d++){
-	rp[d] = ei[d] * DX;
-	sp[d] = r0[d] + rp[d];
-	sp[d] = fmod(sp[d] + lbox[d], lbox[d]);
-	edge[d] = (int)(sp[d]/DX);
-	cp[d] = sp[d] - edge[d]*DX;
-
-	dmy += rp[d]*rp[d];
-      }
-      dmy = sqrt(dmy);
-      for(int d = 0; d < DIM; d++){
-	rp[d] = (dmy > 0 ? rp[d] / dmy : 0.0);
-      }
-      v_gold(ww, rp, dmy, B1_real, B2);
-
-      if(dmy > A){
-	for(int d = 0; d < DIM; d++){
-	  init_interpol(interpol_pid, interpol_pval, interpol_coeff, u[d], edge);
-	  vv[d] = interpol(interpol_pid, interpol_pval, interpol_coeff, cp);
-	}
-      }else{
-	for(int d = 0; d < DIM; d++){
-	  vv[d] = 0.0;
-	}
-      }
-
-      fprintf(fstream, "%3d %3d %8.5f %8.5f %8.5f %8.5f %8.5f %8.5f %8.5f %8.5f %8.5f %8.5f\n",
-	      jj, kk, dmy,
-	      v_inner_prod(vv, ev), v_inner_prod(vv, eu), v_inner_prod(vv, ew),
-	      v_inner_prod(ww, ev), v_inner_prod(ww, eu), v_inner_prod(ww, ew),
-	      v_inner_prod(v0, ev), v_inner_prod(v0, eu), v_inner_prod(v0, ew));
-    }
-  }
-  fclose(fstream);
-}
-
-
 
--- avs_process.h
+++ avs_process.h
@@ -9,23 +9,18 @@
 #include "udfmanager.h"
 #include "macro.h"
 #include "parameter_define.h"
-#include "tricubic.h"
 #include "rigid_body.h"
 #include "quaternion.h"
-enum V_OPTION {relative_velocity, total_velocity};
+#include "tricubic.h"
 enum JAX {x_axis, y_axis, z_axis};
-enum INTERPOLATION {cubic_interpol, trilinear_interpol};
+enum INTERPOLATION {cubic_interpol, trilinear_interpol, no_interpol};
 char AVS_dir[128];      // Main AVS directory
 char Out_dir[128];      // AVS fluid directory
 char Out_name[128];     // AVS fluid filename
 char Outp_dir[128];     // AVS particle directory
 char Outp_name[128];    // AVS particle filename
-char Post_dir[128];     // AVS post-process directory
-char Post_name[128];    // AVS post-process file nameq
 int fluid_veclen;    
 int particle_veclen;
-const int post_veclen = 3;
-const char *Label = "ux uy uz";
 
 double ex[DIM] = {1.0, 0.0, 0.0};
 double ey[DIM] = {0.0, 1.0, 0.0};
@@ -34,17 +29,16 @@ double *e1;
 double *e2;
 double *e3;
 
+double rms[4];
+
 //
 FILE *fluid_field;
-FILE *post_field;
 FILE *particle_field;
 //
 FILE *fluid_cod;
-FILE *post_cod;
 FILE *particle_cod;
 //
 FILE *fluid_data;
-FILE *post_data;
 FILE *particle_data;
 
 int GTS;                // steps between avs dumps
@@ -65,7 +59,8 @@ int &HCY = HCs[1];
 int &HCZ = HCs[2];
 int Nspec;                 // number of species
 int Nparticles;            // total number of particles
-double **u, **post_u;      //
+double **u;
+double UP;
 double B1_real;
 double B1_app;
 double B2;
@@ -80,6 +75,23 @@ double Q0[DIM][DIM];
 quaternion q0;
 INTERPOLATION SW_INTERPOL;
 
+inline void Particle_cell(const double *xp,
+			 const double &dx,
+			 int *x_int,
+			 double *residue){
+  const double idx = 1.0/dx;
+  {
+    for(int d = 0; d < DIM; d++){
+      assert(xp[d] >= 0 && xp[d] < lbox[0]);
+    }
+    for(int d = 0; d < DIM; d++){
+      double dmy = (xp[d] * idx);
+      x_int[d] = (int) dmy;
+
+      residue[d] = (dmy - (double)x_int[d]) * dx;
+    }
+  }
+}
 
 inline double H(const double x){
   return x > 0 ? exp(-SQ(DX/x)) : 0;
@@ -109,6 +121,23 @@ inline int linear_id(const int &i, const int &j, const int &k){
 inline int linear_id(const int &i, const int &j, const int &k, 
 		     const int &NX, const int &NY, const int &NZ){
   return (i * NY * NZ) + (j * NZ) + k;
+}
+inline void Distance(const double *x1,
+		     const double *x2,
+		     double &r12,
+		     double *x12){
+  double dmy = 0.0;
+  for(int d = 0; d < DIM; d++){
+    x12[d] = x2[d] - x1[d];
+    x12[d] -= (double)Nint(x12[d]/lbox[d]) * lbox[d];
+    dmy += SQ(x12[d]);    
+  }
+  r12 = sqrt(dmy);
+  if(dmy > 0){
+    for(int d = 0; d < DIM; d++){
+      x12[d] /= r12;
+    }
+  }
 }
 void get_filename(char* line, char*name){
   char dmy_char[256];
@@ -198,12 +227,10 @@ void clear_avs_frame(){
   fclose(fluid_data);
   fclose(particle_data);
 
-  fclose(post_data);
 
   fluid_cod = NULL;
   fluid_data = NULL;
   particle_data = NULL;
-  post_data = NULL;
 }
 void setup_avs_frame(){
   //find files for new frame
@@ -214,11 +241,9 @@ void setup_avs_frame(){
   // step header
   getline(&line, &len, particle_field);
   getline(&line, &len, fluid_field);
-  fprintf(post_field, "%s", line);
 
   char dmy_fluid[256], fluid_path[256];
   char dmy_particle[256], particle_path[256];
-  char dmy_post[256], post_path[256];
 
   // coord file
   for(int i = 0; i < DIM; i++){
@@ -227,10 +252,6 @@ void setup_avs_frame(){
 
     getline(&line, &len, particle_field); 
     get_filename(line, dmy_particle);
-
-
-    fprintf(post_field, "coord %d file = %s.cod filetype = ascii skip = %d\n",
-	    i+1, Post_name, i*2);
   }
   sprintf(fluid_path, "%s/%s", AVS_dir, dmy_fluid);
   sprintf(particle_path, "%s/%s", AVS_dir, dmy_particle);
@@ -257,20 +278,6 @@ void setup_avs_frame(){
   sprintf(particle_path, "%s/%s", AVS_dir, dmy_particle);
   particle_data = filecheckopen(particle_path, "r");          //particle data file
   //  fprintf(stderr, "%32s  ", particle_path);
-
-  //OUTPUT field data file
-  int data_size = sizeof(float)*CX*CY*CZ;
-  dmy_string.assign(dmy_fluid);
-  start = dmy_string.find_first_not_of(Out_dir);
-  sprintf(dmy_post, "%s/post_%s", Post_dir, dmy_string.substr(start+1).c_str());
-  for(int i=0; i < post_veclen; i++){
-    fprintf(post_field, "variable %d file = %s filetype = binary skip = %d\n", 
-	    i+1, dmy_post, i *data_size);
-  }
-  fprintf(post_field, "EOT\n");
-  sprintf(post_path, "%s/%s", AVS_dir, dmy_post);
-  post_data = filecheckopen(post_path, "w");                  //fluid post data file 
-  //  fprintf(stderr, "%42s\n", post_path);
 }
 
 void initialize_avs(){
@@ -320,41 +327,13 @@ void initialize_avs(){
   getline(&line, &len, particle_field); //nstep
   getline(&line, &len, particle_field); //label
 
-  sprintf(dmy_char, "%s/%s.fld", AVS_dir, Post_name);
-  post_field = filecheckopen(dmy_char, "w");
-  fprintf(post_field, "# AVS field file\n");
-  fprintf(post_field, "ndim=%d\n", DIM);
-  fprintf(post_field, "dim1=%d\n", Cs[0]);
-  fprintf(post_field, "dim2=%d\n", Cs[1]);
-  fprintf(post_field, "dim3=%d\n", Cs[2]);
-  fprintf(post_field, "nspace=%d\n", DIM);
-  fprintf(post_field, "veclen=%d\n", post_veclen);
-  fprintf(post_field, "data=float\n");
-  fprintf(post_field, "field=uniform\n");
-  fprintf(post_field, "nstep=%d\n", Num_snap+1);
-  fprintf(post_field, "label = %s\n", Label);
-
-  //write post fluid .cod  file
-  sprintf(dmy_char, "%s/%s.cod", AVS_dir, Post_name);
-  FILE *dmy_post = filecheckopen(dmy_char, "w");
-  int dmy_start = 0;
-  fprintf(dmy_post, "%g\n%g\n%g\n%g\n%g\n%g\n",
-	  (double)dmy_start*DX,
-	  (double)(Cs[0]-1)*DX,
-	  (double)dmy_start*DX,
-	  (double)(Cs[1]-1)*DX,
-	  (double)dmy_start*DX,
-	  (double)(Cs[2]-1)*DX
-	  );
-  fclose(dmy_post);
 }
 
 void wrong_invocation(){
-  fprintf(stderr, "usage: pavs -p PID -l Ns -i UDF [-v]\n");
+  fprintf(stderr, "usage: pavs -p PID -l Ns -i UDF\n");
   fprintf(stderr, "       -p PID\t ID (1...N) of centered particle\n");
   fprintf(stderr, "       -l Ns \t Box size length\n");
   fprintf(stderr, "       -i UDF\t Input file\n");
-  fprintf(stderr, "       -v    \t Gives relative velocity (co-moving frame)\n");
   exit_job(EXIT_FAILURE);
 }
 void get_system_data(UDFManager *ufin, int *&p_spec, JAX *&sp_axis, double *&sp_slip, double *&sp_slipmode){
@@ -462,24 +441,20 @@ void get_system_data(UDFManager *ufin, int *&p_spec, JAX *&sp_axis, double *&sp_
     strcpy(Out_dir, str.c_str());
     sprintf(Outp_dir, "%s", Out_dir);
     sprintf(Outp_name, "%sp", Out_name);
-    sprintf(Post_dir, "post_%s", Out_dir);
-    sprintf(Post_name, "post_%s", Out_name);
 
     fprintf(stderr, " #Avs directory :  %15s\n", AVS_dir);
-    fprintf(stderr, " #data directory : %15s %15s %15s \n", Out_dir, Outp_dir, Post_dir);
-    fprintf(stderr, " #data filename  : %15s %15s %15s \n\n", Out_name, Outp_name, Post_name);
+    fprintf(stderr, " #data directory : %15s %15s \n", Out_dir, Outp_dir);
+    fprintf(stderr, " #data filename  : %15s %15s \n\n", Out_name, Outp_name);
   }
 }
 void initialize(int argc, char *argv[], 
 		UDFManager* &udf_in,
-		int &pid, 
-		V_OPTION &vflag){
+		int &pid){
   
   char* fname;
   int pset, lset, fset, vset;
   pset = lset = fset = vset = 0;
-  vflag = total_velocity;
-  SW_INTERPOL = cubic_interpol;
+  SW_INTERPOL = no_interpol;
   for(int i = 1; i < argc; i++){
     if(strcmp(argv[i], "-p") == 0){
       if(i+1 == argc || argv[i+1][0] == '-'){
@@ -521,11 +496,6 @@ void initialize(int argc, char *argv[],
 	fprintf(stderr, "Error: Cannot open file\n");
       }
       fset = 1;
-    }else if(strcmp(argv[i], "-v") == 0){
-      vflag = relative_velocity;
-    }else if(strcmp(argv[i], "-nc") == 0){
-      fprintf(stderr, "### LINEAR INTERPOLATION\n");
-      SW_INTERPOL = trilinear_interpol;
     }
   }
 
@@ -548,29 +518,9 @@ void initialize(int argc, char *argv[],
   fprintf(stderr, " # HBox Size        : (%d,%d,%d)\n", 
 	  HCs[0], HCs[1], HCs[2]);
   fprintf(stderr, " # UDF File         : %s\n", fname);
-  fprintf(stderr, " # Relative Velocity: %s\n\n", 
-	  (vflag == relative_velocity) ? "YES" : "NO");
-
 }
 
-
-inline void Particle_cell(const double *xp,
-			 const double &dx,
-			 int *x_int,
-			 double *residue){
-  const double idx = 1.0/dx;
-  {
-    for(int d = 0; d < DIM; d++){
-      assert(xp[d] >= 0 && xp[d] < lbox[0]);
-    }
-    for(int d = 0; d < DIM; d++){
-      double dmy = (xp[d] * idx);
-      x_int[d] = (int) dmy;
-
-      residue[d] = (dmy - (double)x_int[d]) * dx;
-    }
-  }
-}
+////////////////////////
 inline double trilinear_eval(const double pval[8], double &x, double &y, double &z){
   double pint = (pval[0] * (1.0 - x) * (1.0 - y) * (1.0 -z))
     + (pval[1] * x * (1.0 - y) * (1.0 - z))
@@ -668,58 +618,15 @@ inline double interpol(const int pid[8][DIM], const double pval[8], double pcoef
   }
 }
 
-void process_avs_frame(const V_OPTION &vflag, const int &frame){
-  double rp[DIM], sp[DIM], cp[DIM];
-  int ei[DIM], edge[DIM];
-  int im;
-  
-  int interpol_pid[8][DIM];
-  double interpol_pval[8];
-  double interpol_coeff[64];
-
-  for(int i = -HCX; i <= HCX; i++){
-    ei[0] = i;
-    for(int j = -HCY; j <= HCY; j++){
-      ei[1] = j;
-      for(int k = -HCZ; k <= HCZ; k++){
-	ei[2] = k;
-
-	im = ((i + HCX) * CY * CZ) + ((j + HCY) * CZ) + (k + HCZ);
-
-	for(int d = 0; d < DIM; d++){
-	  rp[d] = (double)ei[d] * DX;     //particle frame
-	  sp[d] = r0[d] + rp[d];          //global frame
-	  sp[d] = fmod(sp[d] + lbox[d], lbox[d]);
-	  edge[d] = (int)(sp[d]/DX);
-	  cp[d] = sp[d] - edge[d]*DX;
-
-	  if(!(edge[d] >= 0 && edge[d] < Ns[d] && cp[d] >= 0)){
-	    fprintf(stderr, "rp  : %f\n", rp[d]);
-	    fprintf(stderr, "sp  : %f\n", sp[d]);
-	    fprintf(stderr, "edge: %d\n", edge[d]);
-	    fprintf(stderr, "cp  : %g\n", cp[d]);
-	    assert(false);
-	  }
-	}
-
-	for(int d = 0; d < DIM; d++){
-	  init_interpol(interpol_pid, interpol_pval, interpol_coeff, u[d], edge);
-	  post_u[d][im] = interpol(interpol_pid, interpol_pval, interpol_coeff, cp);
-	  if(vflag == relative_velocity){
-	    post_u[d][im] -= v0[d];
-	  }
-	}
+inline void binary_write(double *a, FILE *fstream){
+  for(int k = 0; k < CZ; k++){
+    for(int j = 0; j < CY; j++){
+      for(int i = 0; i < CX; i++){
+	int im = linear_id(i, j, k, CX, CY, CZ);
+	float dmy = (float)a[im];
+	fwrite(&dmy, sizeof(float), 1, fstream);
       }
     }
   }
-  double jax[DIM];
-  rigid_body_rotation(jax, e3, q0, BODY2SPACE);
-  fprintf(stderr, "# %d: B= %7.5g %7.5g %7.5g\n", 
-	  frame, 
-	  B1_app, B1_real, B2);
-  
-  binary_write(u[0], post_data);
-  binary_write(u[1], post_data);
-  binary_write(u[2], post_data);
 }
 #endif
