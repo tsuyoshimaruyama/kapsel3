133609c35f26e982d4ebb5199b915e76f363758a
john <john@cheme.kyoto-u.ac.jp>
2012-07-26 18:52:05 +0900

    Cleanup to get_xyz code
    -Removed unused command line options
    -Update to read janus axis for udf input
    -Update to incorporate no_pbc coordinates
    -Updated Makefile to compile on linux cluster + omp + mkl

--- get_xyz.cxx
+++ get_xyz.cxx
@@ -11,14 +11,11 @@
 #define MAXBUFFER 100
 #define NDIM 3
 using namespace std;
-enum ID_OPTION_P {POSITION, ORIENTATION, ALL_POS, NO_POS};
-enum ID_OPTION_V {VELOCITY, OMEGA, ALL_VEL, NO_VEL};
-enum ID_OPTION_F {FORCE, TORQUE, ALL_FRC, NO_FRC};
-enum ID_OPTION_N {JANUS_X, JANUS_Y, JANUS_Z, NO_JANUS};
-enum OPTION {in_pos = 1, in_vel = 2, in_frc = 3, in_nt=4, in_fin = 5, in_fout = 6};
+enum OPTION {in_fin=1, in_fout=2};
 double ex[NDIM] = {1.0, 0.0, 0.0};
 double ey[NDIM] = {0.0, 1.0, 0.0};
 double ez[NDIM] = {0.0, 0.0, 1.0};
+double e_none[NDIM] = {0.0, 0.0, 0.0};
 
 void newframe(ofstream &outfile,
 	      double &t,
@@ -29,6 +26,7 @@ void write_xyz(ofstream &outfile,
 	       const int &pid,
 	       const int &sid,
 	       double r[NDIM], 
+	       double r_raw[NDIM],
 	       double QR[NDIM][NDIM],
 	       double v[NDIM],
 	       double vs[NDIM],
@@ -39,40 +37,15 @@ void write_xyz(ofstream &outfile,
 	       double tau_slip[NDIM],
 	       double ni[NDIM],
 	       double n0[NDIM],
-	       int &ntot,
-	       ID_OPTION_P &pflag,
-	       ID_OPTION_V &vflag,
-	       ID_OPTION_F &fflag,
-	       ID_OPTION_N &nflag
-	       );
+	       int &ntot);
 
-void init_xyz(ofstream &outfile, char *fname, ID_OPTION_P &pflag, ID_OPTION_V &vflag, ID_OPTION_F &fflag,
-	      ID_OPTION_N &nflag);
+void init_xyz(ofstream &outfile, char *fname);
 
 void close_xyz(ofstream &outfile);
 
 
 inline void wrong_invocation(){
-  cout << "Usage: get_xyz -OPT_pos -OPT_vel -OPT_frc -OPT_janus UDFfile XYZfile" << endl;
-  cout << "Options:" << endl;
-  cout << "OPT_pos:" << endl;
-  cout << "  -r\t positions" << endl;
-  cout << "  -q\t orientations" << endl;
-  cout << "  -rq\t all pos/orientation info" << endl;
-  cout << endl;
-  cout << "OPT_vel:" << endl;
-  cout << "  -v\t linear velocities" << endl;
-  cout << "  -w\t angular velocities" << endl;
-  cout << "  -vw\t all velocity infro" << endl;
-  cout << endl;
-  cout << "OPT_frc:" << endl;
-  cout << "  -f\t hydrodynamic forces" << endl;
-  cout << "  -t\t hydrodynamic torques" << endl;
-  cout << "  -ft\t all force/torque info" << endl;
-  cout << "OPT_janus:" << endl;
-  cout << "  -jx\t janus x axis" << endl;
-  cout << "  -jy\t janus y axis" << endl;
-  cout << "  -jz\t janus z axis" << endl;
+  cout << "Usage: get_xyz UDFfile XYZfile" << endl;
   exit(1);
 }
 
@@ -80,85 +53,9 @@ int main(int argc, char* argv[])
 {
   
   //check invocation
-  ID_OPTION_P idp = NO_POS;
-  ID_OPTION_V idv = NO_VEL;
-  ID_OPTION_F idf = NO_FRC;
-  ID_OPTION_N idn = NO_JANUS;
-  double * janus_axis = NULL;
-  
-  if(argc != 7){
-    wrong_invocation();
-  }
-  if(strcmp(argv[in_pos], "-r") == 0){
-    idp = POSITION;
-    cout << "Print position" << endl;
-  }else if(strcmp(argv[in_pos], "-q") == 0){
-    idp = ORIENTATION;
-    cout << "Print orientation" << endl;
-  }else if(strcmp(argv[in_pos], "-rq") == 0){
-    idp = ALL_POS;
-    cout << "Print all position info" << endl;
-  }else if(strcmp(argv[in_pos], "-n")){
-    idp = NO_POS;
-    cout << "Skipping position info" << endl;
-  }else{
-    wrong_invocation();
-  }
 
-  if(strcmp(argv[in_vel], "-v") == 0){
-    idv = VELOCITY;
-    cout << "Print velocity" << endl;
-  }else if(strcmp(argv[in_vel], "-w") == 0){
-    idv = OMEGA;
-    cout << "Print angular velocity" << endl;
-  }else if(strcmp(argv[in_vel], "-vw") == 0){
-    idv = ALL_VEL;
-    cout << "Print all velocity info" << endl;
-  }else if(strcmp(argv[in_vel], "-n") == 0){
-    idv = NO_VEL;
-    cout << "Skipping velocity info" << endl;
-  }else{
+  if(argc != 3){
     wrong_invocation();
-  }
-
-  if(strcmp(argv[in_frc], "-f") == 0){
-    idf = FORCE;
-    cout << "Print hydrodynamic force" << endl;
-  }else if(strcmp(argv[in_frc], "-t") == 0){
-    idf = TORQUE;
-    cout << "Print hydrodynamic torque" << endl;
-  }else if(strcmp(argv[in_frc], "-ft") == 0){
-    idf = ALL_FRC;
-    cout << "Print hydrodynamic force info" << endl;
-  }else if(strcmp(argv[in_frc], "-n") == 0){
-    idf = NO_FRC;
-    cout << "Skipping force info" << endl;
-  }else{
-    wrong_invocation();
-  }
-
-  if(strcmp(argv[in_nt], "-jx") == 0){
-    idn = JANUS_X;
-    janus_axis = ex;
-    cout << "Print Janus x axis" << endl;
-  }else if(strcmp(argv[in_nt], "-jy") == 0){
-    idn = JANUS_Y;
-    janus_axis = ey;
-    cout << "Print Janus y axis" << endl;
-  }else if(strcmp(argv[in_nt], "-jz") == 0){
-    idn = JANUS_Z;
-    janus_axis = ez;
-    cout << "Print Janus z axis" << endl;
-  }else if(strcmp(argv[in_nt], "-n") == 0){
-    idn = NO_JANUS;
-    cout << "Skipping Janus info" << endl;
-  }else{
-    wrong_invocation();
-  }
-
-  if(idp == NO_POS && idv == NO_VEL && idf == NO_FRC && idn == NO_JANUS){
-    cout << "Nothing to do" << endl;
-    exit(0);
   }
 
   //make sure udf file exists
@@ -167,7 +64,7 @@ int main(int argc, char* argv[])
     ufin = new UDFManager(argv[in_fin]);
   }
   ofstream outfile;
-  init_xyz(outfile, argv[in_fout], idp, idv, idf, idn);
+  init_xyz(outfile, argv[in_fout]);
   
 
   int nx, ny, nz;
@@ -176,9 +73,9 @@ int main(int argc, char* argv[])
   int nspec;
   int *pnum;
   int *spec_id;
+  double **janus_axis;
   double *bslip;
   const string sep = "\t\t";  
-
   {
     //mesh size
     Location target("mesh");
@@ -203,15 +100,32 @@ int main(int argc, char* argv[])
 
     pnum = alloc_1d_int(nspec);
     bslip = alloc_1d_double(nspec);
+    janus_axis = (double **) malloc(sizeof(double*) * nspec);
     int nmax = 0;
     ntotal = 0;
     for(int i = 0; i < nspec; i++){
       char str[256];
+      string dmy_str;
       sprintf(str, "object_type.spherical_particle.Particle_spec[%d]",i);
       Location target(str);
       ufin -> get(target.sub("Particle_number"), pnum[i]);
       ufin -> get(target.sub("janus_slip_vel"), bslip[i]);
-      fprintf(stderr, "SLIIIIP: %10.8g ", bslip[i]);
+      ufin -> get(target.sub("janus_axis"), dmy_str);
+      if(dmy_str == "X"){
+	janus_axis[i] = ex;
+      }else if(dmy_str == "Y"){
+	janus_axis[i] = ey;
+      }else if(dmy_str == "Z"){
+	janus_axis[i] = ez;
+      }else if(dmy_str == "NONE"){
+	janus_axis[i] = e_none;
+      }else{
+	fprintf(stderr, "Error: Unknown janus axis\n");
+	exit_job(EXIT_FAILURE);
+      }
+      fprintf(stderr, "JANUS: %3.2f %3.2f %3.2f\n", 
+	      janus_axis[i][0], janus_axis[i][1], janus_axis[i][2]);
+      fprintf(stderr, "V_SLIP: %10.8g ", bslip[i]);
       bslip[i] = (ABS(bslip[i]) > 0 ? 1.0 / (2./3 * bslip[i]) : 0.0);
       fprintf(stderr, " (%10.8g) \n", bslip[i]);
       nmax = MAX(nmax, pnum[i]);
@@ -237,6 +151,7 @@ int main(int argc, char* argv[])
   {
     //read particle data
     double r[NDIM];
+    double r_raw[NDIM];
     double v[NDIM];
     double vs[NDIM];
     double w[NDIM];
@@ -262,71 +177,67 @@ int main(int argc, char* argv[])
 	sprintf(str, "Particles[%d]", j);
 	Location target(str);
 
-	if(idp == POSITION || idp == ALL_POS){
-	  ufin -> get(target.sub("R.x"), r[0]);
-	  ufin -> get(target.sub("R.y"), r[1]);
-	  ufin -> get(target.sub("R.z"), r[2]);
+	// position
+	ufin -> get(target.sub("R.x"), r[0]);
+	ufin -> get(target.sub("R.y"), r[1]);
+	ufin -> get(target.sub("R.z"), r[2]);
+	ufin -> get(target.sub("R_raw.x"), r_raw[0]);
+	ufin -> get(target.sub("R_raw.y"), r_raw[1]);
+	ufin -> get(target.sub("R_raw.z"), r_raw[2]);
+	
+	// position no pbc
+	ufin -> get(target.sub("q.q0"), q0);
+	ufin -> get(target.sub("q.q1"), q1);
+	ufin -> get(target.sub("q.q2"), q2);
+	ufin -> get(target.sub("q.q3"), q3);
+	qtn_init(q, q0 ,q1, q2, q3);
+	qtn_isnormal(q, HUGE_TOL_MP);
+	rqtn_rm(QR, q);
+
+	// velocity
+	ufin -> get(target.sub("v.x"), v[0]);
+	ufin -> get(target.sub("v.y"), v[1]);
+	ufin -> get(target.sub("v.z"), v[2]);
+	for(int d = 0; d < NDIM; d++){
+	  vs[d] = v[d] * bslip[spec_id[j]];
 	}
 	
-	if(idp == ORIENTATION || idp == ALL_POS){
-	  ufin -> get(target.sub("q.q0"), q0);
-	  ufin -> get(target.sub("q.q1"), q1);
-	  ufin -> get(target.sub("q.q2"), q2);
-	  ufin -> get(target.sub("q.q3"), q3);
-	  qtn_init(q, q0 ,q1, q2, q3);
-	  qtn_isnormal(q, HUGE_TOL_MP);
-	  rqtn_rm(QR, q);
+	// angular velocity
+	ufin -> get(target.sub("omega.x"), w[0]);
+	ufin -> get(target.sub("omega.y"), w[1]);
+	ufin -> get(target.sub("omega.z"), w[2]);
+	
+	// forces
+	ufin -> get(target.sub("f_hydro.x"), frc[0]);
+	ufin -> get(target.sub("f_hydro.y"), frc[1]);
+	ufin -> get(target.sub("f_hydro.z"), frc[2]);
+	
+	ufin -> get(target.sub("f_slip.x"), frc_slip[0]);
+	ufin -> get(target.sub("f_slip.y"), frc_slip[1]);
+	ufin -> get(target.sub("f_slip.z"), frc_slip[2]);
+	
+	// torques
+	ufin -> get(target.sub("torque_hydro.x"), tau[0]);
+	ufin -> get(target.sub("torque_hydro.y"), tau[1]);
+	ufin -> get(target.sub("torque_hydro.z"), tau[2]);
+	
+	ufin -> get(target.sub("torque_slip.x"), tau_slip[0]);
+	ufin -> get(target.sub("torque_slip.y"), tau_slip[1]);
+	ufin -> get(target.sub("torque_slip.z"), tau_slip[2]);
+	
+
+	ufin -> get(target.sub("q.q0"), q0);
+	ufin -> get(target.sub("q.q1"), q1);
+	ufin -> get(target.sub("q.q2"), q2);
+	ufin -> get(target.sub("q.q3"), q3);
+	qtn_init(q, q0, q1, q2, q3);
+	qtn_isnormal(q, HUGE_TOL_MP);
+	qtn_normalize(q);
+	rigid_body_rotation(ni, janus_axis[spec_id[j]], q, BODY2SPACE);
+	if(i == 0){
+	  v_copy(n0, ni);
 	}
-
-	if(idv == VELOCITY || idv == ALL_VEL){
-	  ufin -> get(target.sub("v.x"), v[0]);
-	  ufin -> get(target.sub("v.y"), v[1]);
-	  ufin -> get(target.sub("v.z"), v[2]);
-	  for(int d = 0; d < NDIM; d++){
-	    vs[d] = v[d] * bslip[spec_id[j]];
-	  }
-	}
-
-	if(idv == OMEGA || idv == ALL_VEL){
-	  ufin -> get(target.sub("omega.x"), w[0]);
-	  ufin -> get(target.sub("omega.y"), w[1]);
-	  ufin -> get(target.sub("omega.z"), w[2]);
-	}
-
-	if(idf == FORCE || idf == ALL_FRC){
-	  ufin -> get(target.sub("f_hydro.x"), frc[0]);
-	  ufin -> get(target.sub("f_hydro.y"), frc[1]);
-	  ufin -> get(target.sub("f_hydro.z"), frc[2]);
-
-	  ufin -> get(target.sub("f_slip.x"), frc_slip[0]);
-	  ufin -> get(target.sub("f_slip.y"), frc_slip[1]);
-	  ufin -> get(target.sub("f_slip.z"), frc_slip[2]);
-	}
-
-	if(idf == TORQUE || idf == ALL_FRC){
-	  ufin -> get(target.sub("torque_hydro.x"), tau[0]);
-	  ufin -> get(target.sub("torque_hydro.y"), tau[1]);
-	  ufin -> get(target.sub("torque_hydro.z"), tau[2]);
-
-	  ufin -> get(target.sub("torque_slip.x"), tau_slip[0]);
-	  ufin -> get(target.sub("torque_slip.y"), tau_slip[1]);
-	  ufin -> get(target.sub("torque_slip.z"), tau_slip[2]);
-	}
-
-	if(idn != NO_JANUS){
-	  ufin -> get(target.sub("q.q0"), q0);
-	  ufin -> get(target.sub("q.q1"), q1);
-	  ufin -> get(target.sub("q.q2"), q2);
-	  ufin -> get(target.sub("q.q3"), q3);
-	  qtn_init(q, q0, q1, q2, q3);
-	  qtn_isnormal(q, HUGE_TOL_MP);
-	  qtn_normalize(q);
-	  rigid_body_rotation(ni, janus_axis, q, BODY2SPACE);
-	  if(i == 0){
-	    v_copy(n0, ni);
-	  }
-	}
-	write_xyz(outfile, t, j+1, spec_id[j], r, QR, v, vs, w, frc, frc_slip, tau, tau_slip, ni, n0, ntotal, idp, idv, idf, idn);
+	write_xyz(outfile, t, j+1, spec_id[j], r, r_raw, QR, v, vs, w, frc, frc_slip, tau, tau_slip, ni, n0, ntotal);
       }
     }
   }
@@ -346,7 +257,8 @@ void write_xyz(ofstream &outfile,
 	       double &t,
 	       const int &pid,
 	       const int &sid,
-	       double r[NDIM], 
+	       double r[NDIM],
+	       double r_raw[NDIM],
 	       double QR[NDIM][NDIM],
 	       double v[NDIM],
 	       double vs[NDIM],
@@ -357,102 +269,85 @@ void write_xyz(ofstream &outfile,
 	       double tau_slip[NDIM],
 	       double ni[NDIM],
 	       double n0[NDIM],
-	       int &ntot,
-	       ID_OPTION_P &pflag,
-	       ID_OPTION_V &vflag,
-	       ID_OPTION_F &fflag,
-	       ID_OPTION_N &nflag){
+	       int &ntot
+	       ){
   char str[4096];
   char dmy_str[256];
   double dmy_ndot = ni[0]*n0[0] + ni[1]*n0[1] + ni[2]*n0[2];
   double dmy_theta = (dmy_ndot < 1.0 ? acos(dmy_ndot) : 0.0);
 
+  // particle info
   sprintf(str, "%d %d ", pid, sid);
-  if(pflag == POSITION || pflag == ALL_POS){
-    sprintf(dmy_str, "%.6g  %.6g  %.6g  ", r[0], r[1], r[2]);
-    strcat(str, dmy_str);
-  }
-  if(vflag == VELOCITY || vflag == ALL_VEL){
-    sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g  ", v[0], v[1], v[2],
-	    sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]),
-	    sqrt(vs[0]*vs[0] + vs[1]*vs[1] + vs[2]*vs[2])
-	    );
-    strcat(str, dmy_str);
-  }
 
-  if(pflag == ORIENTATION || pflag == ALL_POS){
-    sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g  %.6g  %.6g  %.6g  %.6g  ", 
-	    QR[0][0], QR[1][0], QR[2][0],
-	    QR[0][1], QR[1][1], QR[2][1],
-	    QR[0][2], QR[1][2], QR[2][2]);
-    strcat(str, dmy_str);
-  }
-  if(vflag == OMEGA || vflag == ALL_VEL){
-    sprintf(dmy_str, "%.6g  %.6g  %.6g  ", w[0], w[1], w[2]);
-    strcat(str, dmy_str);
-  }
+  // positions
+  sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g %.6g ", r[0], r[1], r[2], r_raw[0], r_raw[1], r_raw[2]);
+  strcat(str, dmy_str);
 
-  if(fflag == FORCE || fflag == ALL_FRC){
-    sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g  %.6g  ", frc[0], frc[1], frc[2], frc_slip[0], frc_slip[1], frc_slip[2]);
-    strcat(str, dmy_str);
-  }
-  if(fflag == TORQUE || fflag == ALL_FRC){
-    sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g  %.6g  ", tau[0], tau[1], tau[2], tau_slip[0], tau_slip[1], tau_slip[2]);
-    strcat(str, dmy_str);
-  }
-  if(nflag != NO_JANUS){
-    sprintf(dmy_str, "%.6g %.6g %.6g %.15g %.6g", ni[0], ni[1], ni[2], dmy_ndot, dmy_theta * 180.0 / M_PI);
-    strcat(str, dmy_str);
-  }
+  // velocities
+  sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g ", v[0], v[1], v[2],
+	  sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]),
+	  sqrt(vs[0]*vs[0] + vs[1]*vs[1] + vs[2]*vs[2])
+	  );
+  strcat(str, dmy_str);
 
-  if(pflag != POSITION && pflag != ORIENTATION && pflag != ALL_POS && pflag != NO_POS){
-    cout << "Wrong position flag!" << endl;
-    exit(1);
-  }
-  if(vflag != VELOCITY && vflag != OMEGA && vflag != ALL_VEL && vflag != NO_VEL){
-    cout << "Wrong velocity flag!" << endl;
-    exit(1);
-  }
-  if(fflag != FORCE && fflag != TORQUE && fflag != ALL_FRC && fflag != NO_FRC){
-    cout << "Wrong  force flag!" << endl;
-    exit(1);
-  }
-  if(nflag != JANUS_X && nflag != JANUS_Y && nflag != JANUS_Z && nflag != NO_JANUS){
-    cout << "Wrong janus flag!" << endl;
-    exit(1);
-  }
+  // orientation matrix
+  sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g  %.6g  %.6g  %.6g  %.6g  ", 
+	  QR[0][0], QR[1][0], QR[2][0],
+	  QR[0][1], QR[1][1], QR[2][1],
+	  QR[0][2], QR[1][2], QR[2][2]);
+  strcat(str, dmy_str);
+
+  // angular velocity
+  sprintf(dmy_str, "%.6g  %.6g  %.6g  ", w[0], w[1], w[2]);
+  strcat(str, dmy_str);
+
+  // forces and torques
+  sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g  %.6g  ", frc[0], frc[1], frc[2], frc_slip[0], frc_slip[1], frc_slip[2]);
+  strcat(str, dmy_str);
+  sprintf(dmy_str, "%.6g  %.6g  %.6g  %.6g  %.6g  %.6g  ", tau[0], tau[1], tau[2], tau_slip[0], tau_slip[1], tau_slip[2]);
+  strcat(str, dmy_str);
+
+  // janus vector
+  sprintf(dmy_str, "%.6g %.6g %.6g %.15g %.6g", ni[0], ni[1], ni[2], dmy_ndot, dmy_theta * 180.0 / M_PI);
+  strcat(str, dmy_str);
+
 
   outfile << str << endl;
 }
 
-void init_xyz(ofstream &outfile, char *fname, ID_OPTION_P &pflag, ID_OPTION_V &vflag, ID_OPTION_F &fflag,
-	      ID_OPTION_N &nflag){
+void init_xyz(ofstream &outfile, char *fname){
 
   outfile.open(fname);
-  char str[256];    
-  sprintf(str, "## 1:p_id 2:spec_id ");
-  if(pflag == POSITION || pflag == ALL_POS){
-    strcat(str, "3:r_x 4:r_y 5:r_z ");
-  }
-  if(vflag == VELOCITY || vflag == ALL_VEL){
-    strcat(str, "6:v_x 7:v_y 8:v_z 9:v 10:vs ");
+  char str[512];    
+  int iid=0;
 
-  }
-  if(pflag == ORIENTATION || pflag == ALL_POS){
-    strcat(str, "11:e1_x 12:e1_y 13:e1_z 14:e2_x 15:e2_y 16:e2_z 17:e3_x 18:e3_y 19:e3_z ");
-  }
-  if(vflag == OMEGA || vflag == ALL_VEL){
-    strcat(str, "20:w_x 21:w_y 22:w_z ");
-  }
-  if(fflag == FORCE || fflag == ALL_FRC){
-    strcat(str, "23:F_x 24:F_y 25:F_z 26:Fs_x 27:Fs_y 28:Fs_z ");
-  }
-  if(fflag == TORQUE || fflag == ALL_FRC){
-    strcat(str, "29:N_x 30:N_y 31:N_z 32:Ns_x 33:Ns_y 34:Ns_z ");
-  }
-  if(nflag != NO_JANUS){
-    strcat(str, "35:e_x 36:e_y 37:e_z 38:c_ee 39:theta");
-  }
+  //id
+  sprintf(str, "## 1:p_id 2:spec_id ");
+
+  //position
+  strcat(str, "3:r_x 4:r_y 5:r_z ");
+
+  //position no pbc
+  strcat(str, "6:rr_x 7:rr_y 8:rr_z ");
+
+  //velocities
+  strcat(str, "9:v_x 10:v_y 11:v_z 12:v 13:vs ");
+
+  //orientation matrix
+  strcat(str, "14:e1_x 15:e1_y 16:e1_z 17:e2_x 18:e2_y 19:e2_z 20:e3_x 21:e3_y 22:e3_z ");
+
+  //angular velocity
+  strcat(str, "23:w_x 24:w_y 25:w_z ");
+
+  // forces (hydro/slip)
+  strcat(str, "26:F_x 27:F_y 28:F_z 29:Fs_x 30:Fs_y 31:Fs_z ");
+
+  // torques (hydro/slip)
+  strcat(str, "32:N_x 33:N_y 34:N_z 35:Ns_x 36:Ns_y 37:Ns_z ");
+
+  // janus axis
+  strcat(str, "38:e_x 39:e_y 40:e_z 41:c_ee 42:theta");
+
   outfile << str << endl;
 
 }
