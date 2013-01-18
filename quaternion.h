2af2ed84c5998da158bd2ad53335c08f46f9ddfc
john <john@cheme.kyoto-u.ac.jp>
2013-01-19 00:44:51 +0900

    Updated quaternion/rigid_body code & documentation

--- quaternion.h
+++ quaternion.h
@@ -1,12 +1,10 @@
 /*!
   \file quaternion.h
   \brief Implements simple quaternion algebra
-  \details Details on the notation can be found in 
-  Graf, B. (2008). Quaternions and dynamics. 
-  http://arxiv.org/abs/0811.2889
   \author J. Molina
   \date 2012/03/28
   \version 1.0
+  \see \link page_rigid_body for more details
 */
 #ifndef QUATERNION_H
 #define QUATERNION_H
@@ -61,9 +59,9 @@ inline void qtn_scalar(double &a, const quaternion &q){
   \brief Get vector component of quaternion
  */
 inline void qtn_vector(double v[DIM], const quaternion &q){
-  for(int i = 0; i < DIM; i++){
-    v[i] = q.v[i];
-  }
+  v[0] = q.v[0];
+  v[1] = q.v[1];
+  v[2] = q.v[2];
 }
 
 /*!
@@ -92,9 +90,9 @@ inline void qtn_init(quaternion &q, const double v4[4]){
  */
 inline void qtn_init(quaternion &q, const double &si, const double vi[DIM]){
   q.s = si;
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = vi[i];
-  }
+  q.v[0] = vi[0];
+  q.v[1] = vi[1];
+  q.v[2] = vi[2];
 }
 
 /*!
@@ -102,30 +100,30 @@ inline void qtn_init(quaternion &q, const double &si, const double vi[DIM]){
  */
 inline void qtn_init(quaternion &q, const quaternion &qa){
   q.s = qa.s;
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = qa.v[i];
-  }
+  q.v[0] = qa.v[0];
+  q.v[1] = qa.v[1];
+  q.v[2] = qa.v[2];
 }
 
 /*!
   \brief Scale quaternion: q = alpha*qa
  */
 inline void qtn_scale(quaternion &q, const quaternion &qa, 
-		      const double scale){
-  q.s = qa.s*scale;
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = qa.v[i]*scale;
-  }
+		      const double alpha){
+  q.s = qa.s*alpha;
+  q.v[0] = qa.v[0]*alpha;
+  q.v[1] = qa.v[1]*alpha;
+  q.v[2] = qa.v[2]*alpha;
 }
 
 /*!
   \brief Scale quaternion in place: q = alpha*q
  */
-inline void qtn_scale(quaternion &q, const double scale){
-  q.s *= scale;
-  for(int i = 0; i < DIM; i++){
-    q.v[i] *= scale;
-  }
+inline void qtn_scale(quaternion &q, const double alpha){
+  q.s *= alpha;
+  q.v[0] *= alpha;
+  q.v[1] *= alpha;
+  q.v[2] *= alpha;
 }
 
 /*!
@@ -133,10 +131,10 @@ inline void qtn_scale(quaternion &q, const double scale){
  */
 inline void qtn_add(quaternion &q, const quaternion &qa, 
 		    const quaternion &qb, const double alpha=1.0){
-  q.s = qa.s + alpha * qb.s;
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = qa.v[i] + alpha * qb.v[i];
-  }
+  q.s = qa.s + qb.s*alpha;
+  q.v[0] = qa.v[0] + qb.v[0]*alpha;
+  q.v[1] = qa.v[1] + qb.v[1]*alpha;
+  q.v[2] = qa.v[2] + qb.v[2]*alpha;
 }
 
 /*!
@@ -144,29 +142,29 @@ inline void qtn_add(quaternion &q, const quaternion &qa,
  */
 inline void qtn_add(quaternion &q, const quaternion &qb, 
 		    const double alpha=1.0){
-  q.s += alpha * qb.s;
-  for(int i = 0; i < DIM; i++){
-    q.v[i] += alpha * qb.v[i];
-  }
+  q.s += qb.s*alpha;
+  q.v[0] += qb.v[0]*alpha;
+  q.v[1] += qb.v[1]*alpha;
+  q.v[2] += qb.v[2]*alpha;
 }
 
 /*!
-  \brief Cuaternion conjugate: q = qa*
+  \brief Quaternion conjugate: q = qa*
  */
 inline void qtn_conj(quaternion &q, const quaternion &qa){
   q.s = qa.s;
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = -qa.v[i];
-  }
+  q.v[0] = -qa.v[0];
+  q.v[1] = -qa.v[1];
+  q.v[2] = -qa.v[2];
 }
 
 /*!
-  \brief Cuaternion conjugate in place: q = q*
+  \brief Quaternion conjugate in place: q = q*
  */
 inline void qtn_conj(quaternion &q){
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = -q.v[i];
-  }
+  q.v[0] = -q.v[0];
+  q.v[1] = -q.v[1];
+  q.v[2] = -q.v[2];
 }
 
 /*!
@@ -175,18 +173,13 @@ inline void qtn_conj(quaternion &q){
 inline void qtn_prod(quaternion &q, const quaternion &qa, 
 		     const quaternion &qb,const double alpha=1.0){
 
-  q.s = qa.s * qb.s;
-  q.v[0] = qa.v[1] * qb.v[2] - qa.v[2] * qb.v[1];
-  q.v[1] = qa.v[2] * qb.v[0] - qa.v[0] * qb.v[2];
-  q.v[2] = qa.v[0] * qb.v[1] - qa.v[1] * qb.v[0];
+  //scalar part
+  q.s = alpha*(qa.s*qb.s - (qa.v[0]*qb.v[0] + qa.v[1]*qb.v[1] + qa.v[2]*qb.v[2]));
 
-  for(int i = 0; i < DIM; i++){
-    q.s -= (qa.v[i] * qb.v[i]);
-
-    q.v[i] += (qa.s * qb.v[i] + qb.s * qa.v[i]);
-    q.v[i] *= alpha;
-  }
-  q.s *= alpha;
+  //vector part
+  q.v[0] = alpha*(qa.s*qb.v[0] + qb.s*qa.v[0] + qa.v[1] * qb.v[2] - qa.v[2] * qb.v[1]);
+  q.v[1] = alpha*(qa.s*qb.v[1] + qb.s*qa.v[1] + qa.v[2] * qb.v[0] - qa.v[0] * qb.v[2]);
+  q.v[2] = alpha*(qa.s*qb.v[2] + qb.s*qa.v[2] + qa.v[0] * qb.v[1] - qa.v[1] * qb.v[0]);
 }
 
 /*!
@@ -207,6 +200,13 @@ inline double qtn_norm(const quaternion &q){
 }
 
 /*!
+  \brief Compute quaternion square norm
+ */
+inline double qtn_sqnorm(const quaternion &q){
+  return q.s*q.s + q.v[0]*q.v[0] + q.v[1]*q.v[1] + q.v[2]*q.v[2];
+}
+
+/*!
   \brief Compute norm of scalar part
  */
 inline double qtn_norm_s(const quaternion &q){
@@ -218,13 +218,6 @@ inline double qtn_norm_s(const quaternion &q){
  */
 inline double qtn_norm_v(const quaternion &q){
   return sqrt(q.v[0]*q.v[0] + q.v[1]*q.v[1] + q.v[2]*q.v[2]);
-}
-
-/*!
-  \brief Compute quaternion square norm
- */
-inline double qtn_sqnorm(const quaternion &q){
-  return q.s*q.s + q.v[0]*q.v[0] + q.v[1]*q.v[1] + q.v[2]*q.v[2];
 }
 
 /*!
@@ -280,15 +273,16 @@ inline void qtn_inv(quaternion &q, const double alpha=1.0){
  */
 inline int qtn_cmp(const quaternion &qa, const quaternion &qb, 
 		    const double tol=LARGE_TOL_MP){
-  bool equalq = equal_tol(qa.s, qb.s, tol);
-  for(int i = 0; i < DIM; i++){
-    equalq = equalq && equal_tol(qa.v[i], qb.v[i], tol);
-  }
+  bool equalq = equal_tol(qa.s, qb.s, tol) && 
+    equal_tol(qa.v[0], qb.v[0], tol) &&
+    equal_tol(qa.v[1], qb.v[1], tol) &&
+    equal_tol(qa.v[2], qb.v[2], tol);
   return (equalq ? 1 : 0);
 }
 
 /*!
-  \brief Return sinc function
+  \brief Return sinc(x)=sin(x)/x function\n
+  sinc(x) = 1 - x^2 / 6 + x^4 / 120 - x^6 / 5040 + O(x^8)
  */
 inline double sinc(const double &x){
   double sincx;
@@ -309,24 +303,25 @@ inline double sinc(const double &x){
   }
   return sincx;
 }
+
 /*!
   \brief Compute rotation quaternion given angle and (normal) axis vector
  */
 inline void rv_rqtn(quaternion &q, const double &phi, const double n[DIM]){
-  double sinc_phi,dn;
   
-  dn = n[0]*n[0] + n[1]*n[1] + n[2]*n[2];
+  double dn = n[0]*n[0] + n[1]*n[1] + n[2]*n[2];
   if(!equal_tol(dn, 1.0, HUGE_TOL_MP)){
     fprintf(stderr, "Error: n not normal vector, |n|= %.15f\n", dn);
     exit_job(EXIT_FAILURE);
   }
-  dn = sqrt(dn);
 
-  sinc_phi = sinc(phi/2.0);
-  q.s = cos(phi/2.0);
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = sinc_phi*(phi/2.0 * n[i]);
-  }
+  //use sinc function for increased accuracy at small angles
+  double phi_half = phi/2.0;
+  double sinc_phi_half = sinc(phi_half)*phi_half;
+  q.s = cos(phi_half);
+  q.v[0] = sinc_phi_half * n[0];
+  q.v[1] = sinc_phi_half * n[1];
+  q.v[2] = sinc_phi_half * n[2];
   qtn_normalize(q);
 }
 
@@ -336,39 +331,36 @@ inline void rv_rqtn(quaternion &q, const double &phi, const double n[DIM]){
   \details Rotation angle is given directly by the magnitude of the vector
  */
 inline void rv_rqtn(quaternion &q, const double v[DIM]){
-  double phi;
-  double sinc_phi;
-
-  phi = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-
-  sinc_phi = sinc(phi/2.0);
-  q.s = cos(phi/2.0);
-  for(int i = 0; i < DIM; i++){
-    q.v[i] = sinc_phi*(v[i]/2.0);
-  }
+  //use sinc function for increased accuracy at small angles
+  double phi_half = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])/2.0;
+  double sinc_phi_half = sinc(phi_half)/2.0; //v=n*phi
+  q.s = cos(phi_half);
+  q.v[0] = sinc_phi_half * v[0];
+  q.v[1] = sinc_phi_half * v[1];
+  q.v[2] = sinc_phi_half * v[2];
   qtn_normalize(q);
 }
 
 /*!
-  \brief Compute rotation angle and (normal) vector from rotation quaternion
+  \brief Compute rotation angle and (normal) axis vector from rotation quaternion
  */
-inline void rqtn_rv(double &phi, double v[DIM], const quaternion &q){
+inline void rqtn_rv(double &phi, double n[DIM], const quaternion &q){
 
   qtn_isnormal(q);
-  phi = 2.0*asin(qtn_norm_v(q));
-  qtn_vector(v, q);
+  qtn_vector(n, q);
+  double ds =  sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2]);
+  phi = 2.0*asin(ds);
 
-  double ds =  sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
   if(positive_mp(ds)){
     ds = 1.0/ds;
-    for(int i = 0; i < DIM; i++){
-      v[i] *= ds;
-    }
+    n[0] *= ds;
+    n[1] *= ds;
+    n[2] *= ds;
   }else{
     phi = 0.0;
-    v[0] = 0.0;
-    v[1] = 0.0;
-    v[2] = 1.0;
+    n[0] = 0.0;
+    n[1] = 0.0;
+    n[2] = 1.0;
   }
 }
 
@@ -378,14 +370,14 @@ inline void rqtn_rv(double &phi, double v[DIM], const quaternion &q){
 inline void rqtn_rv(double v[DIM], const quaternion &q){
   qtn_isnormal(q);
   qtn_vector(v, q);
+  double ds = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+  double phi = 2.0*asin(ds);
 
-  double phi = 2.0*asin(qtn_norm_v(q));
-  double ds = sqrt(v[0]*v[0] + v[1]*v[2] + v[2]*v[2]);
   if(positive_mp(ds)){
-    ds = 1.0/ds;
-    for(int i = 0; i < DIM; i++){
-      v[i] *= (ds * phi);
-    }
+    ds = phi/ds;
+    v[0] *= ds;
+    v[1] *= ds;
+    v[2] *= ds;
   }else{
     phi = 0.0;
     v[0] = 0.0;
@@ -406,17 +398,18 @@ inline void rqtn_euler(double &psi, double &theta, double &phi,
   double dmy_cos;
 
   dmy_cos = (1.0 - 2.0*(q2*q2 + q1*q1));
-  if(!equal_tol(ABS(dmy_cos), 1.0, TOL_MP)){ // not at poles
+
+  if(!equal_tol(ABS(dmy_cos), 1.0, TOL_MP)){ // check for gimbal lock
     psi = atan2(2.0*(q1*q3 - q0*q2), 2.0*(q0*q1 + q2*q3));
     theta = acos(dmy_cos);
     phi = atan2(2.0*(q1*q3 + q0*q2), 2.0*(q0*q1 - q2*q3));
-  }else {//poles
-    if(dmy_cos > 0){ //north pole
-      psi = atan2(2.0*(q0*q3 - q1*q2), 1.0 - 2.0 * (q1*q1 + q3*q3));
+  }else {//rotation with respect to z-axis only, set phi=0!
+    if(dmy_cos > 0){ //theta = 0
+      psi = atan2(2.0*(q0*q3 - q1*q2), 1.0 - 2.0 * (q2*q2 + q3*q3));
       theta = 0.0;
       phi = 0.0;
-    }else{ //south pole
-      psi = atan2(2.0*(q1*q2 - q0*q3), 1.0 - 2.0 * (q1*q1 + q3*q3));
+    }else{ //theta = pi
+      psi = atan2(2.0*(q1*q2 - q0*q3), 1.0 - 2.0 * (q2*q2 + q3*q3));
       theta = M_PI;
       phi = 0.0;
     }
@@ -424,14 +417,12 @@ inline void rqtn_euler(double &psi, double &theta, double &phi,
 }
 
 /*!
-  \brief Compute random rotation quaternion (see Numerical Recipes)
+  \brief Compute random rotation quaternion
  */
 inline void random_rqtn(quaternion &q){
-  double u0, u1, u2, u3, scale;
-  RA_circle(u0, u1);
-  RA_circle(u2, u3);
-  scale = sqrt((1.0 - u0*u0 - u1*u1)/(u2*u2 + u3*u3));
-  qtn_init(q, u3*scale, u0, u1, u2*scale);
+  double q0,q1,q2,q3;
+  RA_on_sphere4D(q0,q1,q2,q3);
+  qtn_init(q, q0, q1, q2, q3);
   qtn_isnormal(q);
 }
 
@@ -439,21 +430,22 @@ inline void random_rqtn(quaternion &q){
   \brief Compute rotation matrix given rotation quaternion
  */
 inline void rqtn_rm(double R[DIM][DIM], const quaternion &q){
+  const double &q0 = q.s;
+  const double &q1 = q.v[0];
+  const double &q2 = q.v[1];
+  const double &q3 = q.v[2];
 
-  //  R[0][0] = 1.0 - 2.0 * q.v[1] * q.v[1] - 2.0 * q.v[2] * q.v[2];
-  R[0][0] = q.s*q.s + q.v[0]*q.v[0] - q.v[1]*q.v[1] - q.v[2]*q.v[2];
-  R[0][1] = 2.0 * q.v[0] * q.v[1] - 2.0 * q.s * q.v[2];
-  R[0][2] = 2.0 * q.s * q.v[1] + 2.0 * q.v[0] * q.v[2];
+  R[0][0] = q0*q0 + q1*q1 - q2*q2 - q3*q3;
+  R[0][1] = 2.0*(q1*q2 - q0*q3);
+  R[0][2] = 2.0*(q1*q3 + q0*q2);
+  
+  R[1][0] = 2.0*(q1*q2 + q0*q3);
+  R[1][1] = q0*q0 - q1*q1 + q2*q2 - q3*q3;
+  R[1][2] = 2.0*(q2*q3 - q0*q1);
 
-  R[1][0] = 2.0 * q.v[0] * q.v[1] + 2.0 * q.s * q.v[2];
-  //  R[1][1] = 1.0 - 2.0 * q.v[0] * q.v[0] - 2.0 * q.v[2] * q.v[2];
-  R[1][1] = q.s*q.s - q.v[0] * q.v[0] + q.v[1]*q.v[1] - q.v[2]*q.v[2];
-  R[1][2] = -2.0 * q.s * q.v[0] + 2.0 * q.v[1] * q.v[2];
-
-  R[2][0] = -2.0 * q.s * q.v[1] + 2.0 * q.v[0] * q.v[2];
-  R[2][1] = 2.0 * q.s * q.v[0] + 2.0 * q.v[1] * q.v[2];
-  //  R[2][2] = 1.0 - 2.0 * q.v[0] * q.v[0] - 2.0 * q.v[1] * q.v[1];
-  R[2][2] = q.s*q.s - q.v[0]*q.v[0] - q.v[1]*q.v[1] + q.v[2]*q.v[2];
+  R[2][0] = 2.0*(q1*q3 - q0*q2);
+  R[2][1] = 2.0*(q2*q3 + q0*q1);
+  R[2][2] = q0*q0 - q1*q1 - q2*q2 + q3*q3;
 }
 
 /*!
@@ -461,16 +453,19 @@ inline void rqtn_rm(double R[DIM][DIM], const quaternion &q){
  */
 inline void rm_rqtn(quaternion &q, const double R[DIM][DIM]){
   double qq[4];
+
+  //q_i^2 components
   qq[0] = 1.0 + R[0][0] + R[1][1] + R[2][2];
   qq[1] = 1.0 + R[0][0] - R[1][1] - R[2][2];
   qq[2] = 1.0 - R[0][0] + R[1][1] - R[2][2];
   qq[3] = 1.0 - R[0][0] - R[1][1] + R[2][2];
+  assert(qq[0] >= 0 && qq[1] >= 0 && qq[2] >= 0 && qq[3] >= 0);
 
   double dmy;
-  double mm = ABS(qq[0]);
+  double mm = qq[0];
   int i = 0;
-  for(int d = 1; d < 4; d++){
-    dmy = ABS(qq[d]);
+  for(int d = 1; d < 4; d++){ // find component with largest absolute magnitude
+    dmy = qq[d];
     if(dmy > mm){
       mm = dmy;
       i = d;
@@ -489,28 +484,25 @@ inline void rm_rqtn(quaternion &q, const double R[DIM][DIM]){
     qq[1] = sqrt(qq[1]) / 2.0;
     dmy = 1.0/(4.0 * qq[1]);
 
+    qq[0] = (R[2][1] - R[1][2]) * dmy;
     qq[2] = (R[1][0] + R[0][1]) * dmy;
     qq[3] = (R[0][2] + R[2][0]) * dmy;
-
-    qq[0] = (R[2][1] - R[1][2]) * dmy;
     break;
   case 2:
     qq[2] = sqrt(qq[2]) / 2.0;
     dmy = 1.0/(4.0 * qq[2]);
 
-    qq[3] = (R[2][1] + R[1][2]) * dmy;
-    qq[1] = (R[1][0] + R[0][1]) * dmy;
-
     qq[0] = (R[0][2] - R[2][0]) * dmy;
+    qq[1] = (R[1][0] + R[0][1]) * dmy;
+    qq[3] = (R[2][1] + R[1][2]) * dmy;
     break;
   case 3:
     qq[3] = sqrt(qq[3]) / 2.0;
     dmy = 1.0/(4.0 * qq[3]);
 
+    qq[0] = (R[1][0] - R[0][1]) * dmy;
     qq[1] = (R[2][0] + R[0][2]) * dmy;
     qq[2] = (R[2][1] + R[1][2]) * dmy;
-
-    qq[0] = (R[1][0] - R[0][1]) * dmy;
     break;
   }
   qtn_init(q, qq);
--- rigid_body.cxx
+++ rigid_body.cxx
@@ -8,96 +8,6 @@
   \version 1.0
 */
 
-#define ONE 1.0
-#define ZERO 0.0
-//
-// Get skew anti-symmetric matrix
-inline void skew(double ws[DIM][DIM], 
-		 const double w[DIM]){
-  assert(DIM == 3);
-  ws[0][0] = ZERO;
-  ws[0][1] = -w[2];
-  ws[0][2] = w[1];
-  ws[1][0] = w[2];
-  ws[1][1] = ZERO;
-  ws[1][2] = -w[0];
-  ws[2][0] = -w[1];
-  ws[2][1] = w[0];
-  ws[2][2] = ZERO;
-}
-
-//
-// Make sure x is in body coordinates
-// (orientation matrix)
-void ensure_body_coordinates(double x[DIM],
-			     const double QR[DIM][DIM],
-			     const COORD_SYSTEM &coord){
-  if(coord == BODY_FRAME){
-    return;
-  }else if(coord == SPACE_FRAME){
-    v_M_prod(x, QR);
-  }else {
-    fprintf(stderr, "Error: unknown coordinate system\n");
-    exit_job(EXIT_FAILURE);
-  }
-}
-// (orientation quaternion)
-void ensure_body_coordinates(double x[DIM],
-			     const quaternion &q,
-			     const COORD_SYSTEM &coord){
-  if(coord == BODY_FRAME){
-    return;
-  }else if(coord == SPACE_FRAME){
-    quaternion qx;
-    quaternion q_dmy;
-    qtn_init(qx, ZERO, x);
-    qtn_conj(q_dmy, q);
-
-    qtn_prod(qx, q);
-    qtn_prod(q_dmy, qx);
-    qtn_vector(x, q_dmy);
-  }else{
-    fprintf(stderr, "Error: unknown coordinate system\n");
-    exit_job(EXIT_FAILURE);
-  }
-}
-
-//
-// Make sure x is in space coordinates
-// (orientation  matrix)
-void ensure_space_coordinates(double x[DIM],
-			      const double QR[DIM][DIM],
-			      const COORD_SYSTEM &coord){
-  if(coord == SPACE_FRAME){
-    return;
-  }else if(coord == BODY_FRAME){
-    M_v_prod(x, QR);
-  }else{
-    fprintf(stderr, "Error: unknown coordinate system\n");
-    exit_job(EXIT_FAILURE);
-  }
-}
-// (orientation quaternion)
-void ensure_space_coordinates(double x[DIM],
-			      const quaternion &q,
-			      const COORD_SYSTEM &coord){
-  if(coord == SPACE_FRAME){
-    return;
-  }else if(coord == BODY_FRAME){
-    quaternion qx;
-    quaternion q_dmy;
-    qtn_init(qx, ZERO, x);
-    qtn_conj(q_dmy, q);
-
-    qtn_prod(qx, q_dmy);
-    qtn_prod(q_dmy, q, qx);
-    qtn_vector(x, q_dmy);
-  }else{
-    fprintf(stderr, "Error: unknown coordinate system\n");
-    exit_job(EXIT_FAILURE);
-  }
-}
-
 // Perform rigid body rotation
 // (quaternion)
 void rigid_body_rotation(double rotated[DIM], 
@@ -107,7 +17,7 @@ void rigid_body_rotation(double rotated[DIM],
   quaternion qx;
   quaternion q_dmy;
 
-  qtn_init(qx, ZERO, original);
+  qtn_init(qx, 0.0, original);
   qtn_conj(q_dmy, q);
   
   if(transform == BODY2SPACE){
@@ -149,7 +59,7 @@ void qdot(quaternion &dqdt,
 	  const double omega[DIM], 
 	  const COORD_SYSTEM &coord){
   quaternion qw;
-  qtn_init(qw, ZERO, omega);
+  qtn_init(qw, 0.0, omega); //omega in body or space coord
   if(coord == SPACE_FRAME){
     qtn_prod(dqdt, qw, q, 0.5);
   } else if(coord == BODY_FRAME){
@@ -159,13 +69,6 @@ void qdot(quaternion &dqdt,
     exit_job(EXIT_FAILURE);
   }
 }
-inline void qdot(quaternion &dqdt,
-		 const double omega[DIM],
-		 const COORD_SYSTEM &coord){
-  quaternion q;
-  qtn_init(q, dqdt);
-  qdot(dqdt, q, omega, coord);
-}
 
 //
 // time derivative of orientation matrix
@@ -174,26 +77,16 @@ void Qdot(double dQRdt[DIM][DIM],
 	  const double omega[DIM],
 	  const COORD_SYSTEM &coord){
   
-  double omega_body[DIM];
   double omega_skew[DIM][DIM];
-
+  skew(omega_skew, omega); // omega in body or space coord
   if(coord == SPACE_FRAME){
-    rigid_body_rotation(omega_body, omega, QR, SPACE2BODY);
-    skew(omega_skew, omega_body);
+    M_prod(dQRdt, omega_skew, QR);
   } else if (coord == BODY_FRAME){
-    skew(omega_skew, omega);
+    M_prod(dQRdt, QR, omega_skew);
   } else{
     fprintf(stderr, "Error: unknown coordinate system\n");
     exit_job(EXIT_FAILURE);
   }
-  M_prod(dQRdt, QR, omega_skew);
-}
-inline void Qdot(double dQRdt[DIM][DIM],
-		 const double omega[DIM],
-		 const COORD_SYSTEM &coord){
-  double QR[DIM][DIM];
-  M_copy(QR, dQRdt);
-  Qdot(dQRdt, QR, omega, coord);
 }
 
 // time derivative of angular velocity
@@ -224,12 +117,12 @@ inline void wdot(double dwdt[DIM],
 /*void rodrigues_rotation_formula(double T[DIM][DIM], 
 				const double phi, 
 				const double n[DIM]){
-  double Id[DIM][DIM] = {{ONE, ZERO, ZERO}, 
-			 {ZERO, ONE, ZERO},
-			 {ZERO, ZERO, ONE}};
+  double Id[DIM][DIM] = {{1.0, 0.0, 0.0}, 
+			 {0.0, 1.0, 0.0},
+			 {0.0, 0.0, 1.0}};
   double V[DIM][DIM];
 
-  double cs = ONE - cos(phi);
+  double cs = 1.0 - cos(phi);
   double sn = sin(phi);
   skew(V, n);
   for(int i = 0; i < DIM; i++){
@@ -251,7 +144,7 @@ inline void wdot(double dwdt[DIM],
 		      const double &dt){
   quaternion kq1, kq2, kq3, kq4, dmy_q;
   double kw1[DIM], kw2[DIM], kw3[DIM], kw4[DIM], dmy_w[DIM];
-  double tau[DIM] = {ZERO, ZERO, ZERO};
+  double tau[DIM] = {0.0, 0.0, 0.0};
 
   qtn_init(dmy_q, q);
   v_copy(dmy_w, omega);
@@ -302,7 +195,7 @@ inline void wdot(double dwdt[DIM],
   double rot_operator[DIM][DIM];
   double dmy_QR[DIM][DIM];
   double kw1[DIM], kw2[DIM], kw3[DIM], kw4[DIM], dmy_w[DIM];
-  double tau[DIM] = {ZERO, ZERO, ZERO};
+  double tau[DIM] = {0.0, 0.0, 0.0};
 
   // orientation update
   v_copy(rot_vector, omega);
