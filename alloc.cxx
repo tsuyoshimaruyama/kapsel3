a3c03bde7d0dc467c1846c3185525dedd33da1fa
john <john@cheme.kyoto-u.ac.jp>
2018-01-05 12:10:32 +0900

    cleanup alloc code

--- alloc.cxx
+++ alloc.cxx
@@ -9,99 +9,91 @@
 #include "alloc.h"
 
 template<typename T>  T*  alloc_1d(int n1){
-  T* i = (T*) std::malloc(n1*sizeof(T));
-  alloc_error_check(i);
-  return i;
+  T* p = (T*) std::malloc(n1*sizeof(T));
+  alloc_error_check(p);
+  return p;
 }
-template int*    alloc_1d(int n1);
-template double* alloc_1d(int n1);
-
-template<typename T>  void free_1d(T *i){
-  std::free(i);
+template<typename T>  void free_1d(T* p){
+  std::free(p);
 }
-template void free_1d(int *i);
-template void free_1d(double *i);
 
-
-template<typename T>
-T** initview_2d(int n1, int n2,  T* i){
-  T** ii = (T **) alloc_1d<T*>(n1);
-  alloc_error_check(ii);
-  ii[0] = i;
-  for(int j = 1; j < n1; j++) ii[j] = ii[j-1] + n2;
-  return ii;
+template<typename T> T** initview_2d(int n1, int n2,  T * const p){
+  T** pp = (T **) alloc_1d<T*>(n1);
+  alloc_error_check(pp);
+  pp[0] = p;
+  for(int j = 1; j < n1; j++) pp[j] = pp[j-1] + n2;
+  return pp;
 }
-template int**    initview_2d(int n1, int n2, int* i);
-template double** initview_2d(int n1, int n2, double* i);
-
-template<typename T>
-void freeview_2d(T **ii){
-  free(ii);
+template<typename T> void freeview_2d(T **pp){
+  free_1d<T*>(pp);
 }
-template void freeview_2d(int **ii);
-template void freeview_2d(double **ii);
 
-template<typename T>
-T** alloc_2d(int n1, int n2){
-  T*  i  = (T *)  alloc_1d<T>(n1 * n2);
-  alloc_error_check(i);
-  T** ii = initview_2d(n1, n2, i);
-  ii[0] = i;
-  for(int j = 1; j < n1; j++) ii[j] = ii[j-1] + n2;
-  return ii;
+template<typename T> T** alloc_2d(int n1, int n2){
+  T*  p  = (T *)  alloc_1d<T>(n1 * n2);
+  alloc_error_check(p);
+  T** pp = initview_2d<T>(n1, n2, p);
+  return pp;
 }
-template int**    alloc_2d(int n1, int n2);
-template double** alloc_2d(int n1, int n2);
-
-template<typename T>
-void free_2d(T **ii){
-    free(ii[0]);
-    freeview_2d(ii);
+template<typename T> void free_2d(T **pp){
+  free_1d<T>(pp[0]);
+  freeview_2d<T>(pp);
 }
-template void free_2d(int **ii);
-template void free_2d(double **ii);
 
-template<typename T>
-T*** initview_3d(int n1, int n2, int n3, T* i){
-  T ***iii = (T ***) alloc_1d<T**>(n1);
-  alloc_error_check(iii);
-  T **ii   = (T **)  alloc_1d<T*>(n1 * n2);
-  alloc_error_check(ii);
+template<typename T> T*** initview_3d(int n1, int n2, int n3, T* const p){
+  T ***ppp = (T ***) alloc_1d<T**>(n1);
+  alloc_error_check(ppp);
+  T **pp   = (T **)  alloc_1d<T*>(n1 * n2);
+  alloc_error_check(pp);
 
-  iii[0] = ii;
-  for(int j = 1; j < n1; j++) iii[j] = iii[j-1] + n2;
+  ppp[0] = pp;
+  for(int j = 1; j < n1; j++) ppp[j] = ppp[j-1] + n2;
 
-  ii[0]  = i;
-  for(int j = 1; j < n1*n2; j++) ii[j] = ii[j-1] + n3;
-  return iii;
+  pp[0]  = p;
+  for(int j = 1; j < n1*n2; j++) pp[j] = pp[j-1] + n3;
+  return ppp;
 }
-template int***    initview_3d(int n1, int n2, int n3, int* i);
-template double*** initview_3d(int n1, int n2, int n3, double* i);
-
-template<typename T>
-void freeview_3d(T ***iii){
-  free(iii[0]);
-  free(iii);
+template<typename T> void freeview_3d(T ***ppp){
+  free_1d<T*>(ppp[0]);
+  free_1d<T**>(ppp);
 }
-template void freeview_3d(int ***i);
-template void freeview_2d(double ***i);
 
-template<typename T>
-T ***alloc_3d(int n1, int n2, int n3){
-  T*     i = (T *) alloc_1d<T>(n1 * n2 * n3);
-  alloc_error_check(i);
-  T*** iii = initview_3d(n1, n2, n3, i);
-  return iii;
+template<typename T> T ***alloc_3d(int n1, int n2, int n3){
+  T*     p = (T *) alloc_1d<T>(n1 * n2 * n3);
+  alloc_error_check(p);
+  T*** ppp = initview_3d<T>(n1, n2, n3, p);
+  return ppp;
 }
-template int***    alloc_3d(int n1, int n2, int n3);
-template double*** alloc_3d(int n1, int n2, int n3);
+template<typename T> void free_3d(T ***ppp){
+  free_1d<T>(ppp[0][0]);
+  freeview_3d<T>(ppp);
+}
 
-template<typename T>
-void free_3d(T ***iii){
-    free(iii[0][0]);
-    freeview_3d(iii);
-}
-template void free_3d(int ***iii);
-template void free_3d(double ***iii);
+int*       alloc_1d_int(int n1){return alloc_1d<int>(n1);}
+int**      alloc_2d_int(int n1, int n2){return alloc_2d<int>(n1, n2);}
+int***     alloc_3d_int(int n1, int n2, int n3){return alloc_3d<int>(n1, n2, n3);}
+int**      initview_2d_int(int n1, int n2, int* const i){return initview_2d<int>(n1, n2, i);}
+int***     initview_3d_int(int n1, int n2, int n3, int* const i){return initview_3d<int>(n1, n2, n3, i);}
+
+void       free_1d_int(int *i){return free_1d<int>(i);}
+void       free_2d_int(int **ii){return free_2d<int>(ii);}
+void       free_3d_int(int ***iii){return free_3d<int>(iii);}
+void       freeview_2d_int(int **ii){return freeview_2d<int>(ii);}
+void       freeview_3d_int(int ***iii){return freeview_3d<int>(iii);}
+
+
+double*    alloc_1d_double(int n1){return alloc_1d<double>(n1);}
+double**   alloc_2d_double(int n1, int n2){return alloc_2d<double>(n1, n2);}
+double***  alloc_3d_double(int n1, int n2, int n3){return alloc_3d<double>(n1, n2, n3);}
+double**   initview_2d_double(int n1, int n2, double* const d){return initview_2d<double>(n1, n2, d);}
+double***  initview_3d_double(int n1, int n2, int n3, double* const d){return initview_3d<double>(n1, n2, n3, d);}
+
+void       free_1d_double(double *d){return free_1d<double>(d);}
+void       free_2d_double(double **dd){return free_2d<double>(dd);}
+void       free_3d_double(double ***ddd){return free_3d<double>(ddd);}
+void       freeview_2d_double(double **dd){return freeview_2d<double>(dd);}
+void       freeview_3d_double(double ***ddd){return freeview_3d<double>(ddd);}
+
+
+
 
 
--- alloc.h
+++ alloc.h
@@ -19,29 +19,27 @@
     } \
 }
 
-//extern "C" int posix_memalign(void **memptr, size_t alignment ,size_t size);
-
 /*!
   \brief Allocate memory for 1d array
  */
-template<typename T>   T* alloc_1d(int n1);
+template<typename T> T* alloc_1d(int n1);
 
 /*!
   \brief Free memory for 1d array
  */
-template<typename T>  void free_1d(T *i);
+template<typename T> void free_1d(T *p);
 
 /*!
   \brief Create 2d view of 1d array referenced by i
   \details Allocates array ii of size n1, each element of ii give view of rows of i
   ii[I][J] = i[I*n2 + J]
  */
-template<typename T>   T** initview_2d(int n1, int n2,  T const * const i);
+template<typename T> T** initview_2d(int n1, int n2,  T * const p);
 
 /*!
   \brief Destroy 2d view
  */
-template<typename T> void freeview_2d(T **ii);
+template<typename T> void freeview_2d(T **pp);
 /*!
   \brief Allocate memory for 2d array
   \details Allocates linear array of size n1*n2, position of element
@@ -58,11 +56,11 @@ template<typename T> void free_2d(T **ii);
   \details Allocates array iii of size n1, each iii[.] points to 2D array ii such that
   iii[I][J][K] = i[I*n2*n3 + J*n3 + K]
  */
-template<typename T>  T*** initview_3d(int n1, int n2, int n3, T const * const i);
+template<typename T>  T*** initview_3d(int n1, int n2, int n3, T * const p);
 /*!
   \brief Destroy 3d view
  */
-template<typename T> void  freeview_3d(T ***ii);
+template<typename T> void  freeview_3d(T ***ppp);
 /*!
   \brief Allocate memory for 3d array
   \details Allocates linear array of size n1*n2*n3, position of element
@@ -74,18 +72,29 @@ template<typename T>  T*** alloc_3d(int n1, int n2, int n3);
  */
 template<typename T> void  free_3d(T ***ii);
 
-inline int*      alloc_1d_int(int n1){return alloc_1d<int>(n1); }
-inline int**     alloc_2d_int(int n1, int n2){return alloc_2d<int>(n1, n2);}
-inline int***    alloc_3d_int(int n1, int n2, int n3){return alloc_3d<int>(n1, n2, n3);}
-inline void      free_1d_int(int *i){return free_1d<int>(i);}
-inline void      free_2d_int(int **ii){return free_2d<int>(ii);}
-inline void      free_3d_int(int ***iii){return free_3d<int>(iii);}
+int*      alloc_1d_int(int n1);
+int**     alloc_2d_int(int n1, int n2);
+int***    alloc_3d_int(int n1, int n2, int n3);
+int**     initview_2d_int(int n1, int n2, int * const i);
+int***    initview_3d_int(int n1, int n2, int n3, int * const i);
 
-inline double*   alloc_1d_double(int n1){return alloc_1d<double>(n1);}
-inline double**  alloc_2d_double(int n1, int n2){return alloc_2d<double>(n1, n2);}
-inline double*** alloc_3d_double(int n1, int n2, int n3){return alloc_3d<double>(n1, n2, n3);}
-inline void      free_1d_double(double *d){return free_1d<double>(d);}
-inline void      free_2d_double(double **d){return free_2d<double>(d);}
-inline void      free_3d_double(double ***d){return free_3d<double>(d);}
+void      free_1d_int(int *i);
+void      free_2d_int(int **ii);
+void      free_3d_int(int ***iii);
+void      freeview_2d_int(int **ii);
+void      freeview_3d_int(int ***iii);
+
+double*   alloc_1d_double(int n1);
+double**  alloc_2d_double(int n1, int n2);
+double*** alloc_3d_double(int n1, int n2, int n3);
+double**  initview_2d_double(int n1, int n2, double * const d);
+double*** initview_3d_double(int n1, int n2, int n3, double * const d);
+
+void      free_1d_double(double *d);
+void      free_2d_double(double **dd);
+void      free_3d_double(double ***dddd);
+void      freeview_2d_double(double **dd);
+void      freeview_3d_double(double ***dddd);
+
 
 #endif
